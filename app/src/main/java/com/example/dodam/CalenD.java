474f51f2dd5f3520c3f1a3720a16121d25535c68
Han-YouKyung <becky102832@gmail.com>
2021-06-10 11:37:32 +0900

    다이어리 수정중(DB 안넘어감)

--- app/src/main/java/com/example/dodam/CalenD.java
+++ app/src/main/java/com/example/dodam/CalenD.java
@@ -56,7 +56,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         구현 남은 것 메모
         1. DB에 일기가 저장된 경우 circle visible로 바꾸기 + 가능하면 색깔도 설정
         2. 버튼을 눌렀을 경우 해당 날짜의 일기가 나오도록 하기
-        3. 날짜가 선택된 경우 선택된 날짜 동그라미 보이기
+
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -193,9 +193,18 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
     @Override
     public void onItemClick(AdapterView<?> parent, View v, int position, long arg3)
     {
+
+        for (int i = 0; i <parent.getChildCount(); i++){
+            parent.getChildAt(i).findViewById(R.id.flag).setVisibility(View.INVISIBLE);
+
+        }
+
+        v.findViewById(R.id.flag).setVisibility(View.VISIBLE);
+
         date = mThisMonthCalendar.get(Calendar.YEAR) + ". " +
                 (mThisMonthCalendar.get(Calendar.MONTH) + 1) + ". " +
                 mDayList.get(position).getDay();
+
 
         /*
             일기가 DB에 있으면 일기랑 스탬프 보여주기 !! diary.setText(~); stamp.setImage(~);
