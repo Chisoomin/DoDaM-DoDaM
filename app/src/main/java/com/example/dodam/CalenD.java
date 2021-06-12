b1d7ddd24dcf30c6637812b30d9d244b82727e3d
Han-YouKyung <becky102832@gmail.com>
2021-06-13 01:03:00 +0900

    캘린더 날짜 DB에 잘 들어가게 수정

--- app/src/main/java/com/example/dodam/CalenD.java
+++ app/src/main/java/com/example/dodam/CalenD.java
@@ -3,7 +3,10 @@ package com.example.dodam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -14,20 +17,20 @@ import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 
-public class CalenD extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener
-{
-    public static int SUNDAY        = 1;
-    public static int MONDAY        = 2;
-    public static int TUESDAY       = 3;
-    public static int WEDNSESDAY    = 4;
-    public static int THURSDAY      = 5;
-    public static int FRIDAY        = 6;
-    public static int SATURDAY      = 7;
+public class CalenD extends AppCompatActivity implements AdapterView.OnItemClickListener, View.OnClickListener {
+    public static int SUNDAY = 1;
+    public static int MONDAY = 2;
+    public static int TUESDAY = 3;
+    public static int WEDNSESDAY = 4;
+    public static int THURSDAY = 5;
+    public static int FRIDAY = 6;
+    public static int SATURDAY = 7;
 
     private TextView mTvCalendarTitle;
     private GridView mGvCalendar;
@@ -44,13 +47,18 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
 
     String date; // 선택된 날짜
     String dbDate;
-    /*DB 영역
-    DiaryDBHelper dbHelper = new DiaryDBHelper(this);
+    String month;
+    String day;
+
+    LinearLayout diaryContainer;
+
+    //DB 영역
+   /* DiaryDBHelper dbHelper = new DiaryDBHelper(this);
     SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-    Cursor cursor = db.rawQuery("select strftime('%Y-%m-%d', date), content from DiaryData;", null);
+    Cursor cursor = db.rawQuery("select strftime('%Y-%m-%d', date), content from DiaryData;", null);*/
 
-    //DB 끝*/
+    //DB 끝
 
     /*
         구현 남은 것 메모
@@ -58,16 +66,17 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         2. 버튼을 눌렀을 경우 해당 날짜의 일기가 나오도록 하기
 
      */
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        ImageButton bLastMonth = (ImageButton)findViewById(R.id.mleft);
-        ImageButton bNextMonth = (ImageButton)findViewById(R.id.mright);
+        ImageButton bLastMonth = (ImageButton) findViewById(R.id.mleft);
+        ImageButton bNextMonth = (ImageButton) findViewById(R.id.mright);
 
-        mTvCalendarTitle = (TextView)findViewById(R.id.year);
-        mGvCalendar = (GridView)findViewById(R.id.calendar_grid);
+        mTvCalendarTitle = (TextView) findViewById(R.id.year);
+        mGvCalendar = (GridView) findViewById(R.id.calendar_grid);
 
         diary = (TextView) findViewById(R.id.diary);
         stamp = (ImageView) findViewById(R.id.stamp);
@@ -77,6 +86,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         mGvCalendar.setOnItemClickListener(this);
 
         mDayList = new ArrayList<DayInfo>();
+
 
         diary.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,8 +106,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
     }
 
     @Override
-    protected void onResume()
-    {
+    protected void onResume() {
         super.onResume();
 
         // 이번달 의 캘린더 인스턴스를 생성한다.
@@ -106,8 +115,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         getCalendar(mThisMonthCalendar);
     }
 
-    private void getCalendar(Calendar calendar)
-    {
+    private void getCalendar(Calendar calendar) {
         int lastMonthStartDay;
         int dayOfMonth;
         int thisMonthLastDay;
@@ -119,20 +127,19 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         thisMonthLastDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 
         calendar.add(Calendar.MONTH, -1);
-        Log.e("지난달 마지막일", calendar.get(Calendar.DAY_OF_MONTH)+"");
+        Log.e("지난달 마지막일", calendar.get(Calendar.DAY_OF_MONTH) + "");
 
         // 지난달의 마지막 일자를 구한다.
         lastMonthStartDay = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 
         calendar.add(Calendar.MONTH, 1);
-        Log.e("이번달 시작일", calendar.get(Calendar.DAY_OF_MONTH)+"");
+        Log.e("이번달 시작일", calendar.get(Calendar.DAY_OF_MONTH) + "");
 
-        if(dayOfMonth == SUNDAY)
-        {
+        if (dayOfMonth == SUNDAY) {
             dayOfMonth += 7;
         }
 
-        lastMonthStartDay -= (dayOfMonth-1)-1;
+        lastMonthStartDay -= (dayOfMonth - 1) - 1;
 
 
         // 캘린더 타이틀(년월 표시)을 세팅한다.
@@ -141,28 +148,25 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
 
         DayInfo day;
 
-        Log.e("DayOfMOnth", dayOfMonth+"");
+        Log.e("DayOfMOnth", dayOfMonth + "");
 
 
-        for(int i=0; i<dayOfMonth-1; i++)
-        {
-            int date = lastMonthStartDay+i;
+        for (int i = 0; i < dayOfMonth - 1; i++) {
+            int date = lastMonthStartDay + i;
             day = new DayInfo();
             day.setDay(Integer.toString(date));
             day.setInMonth(false);
 
             mDayList.add(day);
         }
-        for(int i=1; i <= thisMonthLastDay; i++)
-        {
+        for (int i = 1; i <= thisMonthLastDay; i++) {
             day = new DayInfo();
             day.setDay(Integer.toString(i));
             day.setInMonth(true);
 
             mDayList.add(day);
         }
-        for(int i=1; i<42-(thisMonthLastDay+dayOfMonth-1)+1; i++)
-        {
+        for (int i = 1; i < 42 - (thisMonthLastDay + dayOfMonth - 1) + 1; i++) {
             day = new DayInfo();
             day.setDay(Integer.toString(i));
             day.setInMonth(false);
@@ -173,8 +177,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         initCalendarAdapter();
     }
 
-    private Calendar getLastMonth(Calendar calendar)
-    {
+    private Calendar getLastMonth(Calendar calendar) {
         calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 1);
         calendar.add(Calendar.MONTH, -1);
         mTvCalendarTitle.setText(mThisMonthCalendar.get(Calendar.YEAR) + ". "
@@ -182,8 +185,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         return calendar;
     }
 
-    private Calendar getNextMonth(Calendar calendar)
-    {
+    private Calendar getNextMonth(Calendar calendar) {
         calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), 1);
         calendar.add(Calendar.MONTH, +1);
         mTvCalendarTitle.setText(mThisMonthCalendar.get(Calendar.YEAR) + ". "
@@ -191,23 +193,50 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         return calendar;
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View v, int position, long arg3)
-    {
+   /* //DB 영역
+    DiaryDBHelper dbHelper = new DiaryDBHelper(this);
+    SQLiteDatabase db = dbHelper.getReadableDatabase();
 
-        for (int i = 0; i <parent.getChildCount(); i++){
+    Cursor cursor = db.rawQuery("select strftime('%Y-%m-%d', date), content from DiaryData;", null);
+
+    *///DB 끝
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View v, int position, long arg3) {
+
+        for (int i = 0; i < parent.getChildCount(); i++) {
             parent.getChildAt(i).findViewById(R.id.flag).setVisibility(View.INVISIBLE);
 
         }
 
         v.findViewById(R.id.flag).setVisibility(View.VISIBLE);
 
+        if ((mThisMonthCalendar.get(Calendar.MONTH) + 1) <= 9 && (mThisMonthCalendar.get(Calendar.MONTH) + 1) >= 1) {
+            month = "0" + (mThisMonthCalendar.get(Calendar.MONTH) + 1);
+        }
+        //Integer.toString(from);
+
+        if ((Integer.parseInt(String.valueOf(mDayList.get(position).getDay()))) <= 9 && (Integer.parseInt(String.valueOf(mDayList.get(position).getDay())) >= 1)) {
+            day = "0" + mDayList.get(position).getDay();
+        }
+
         date = mThisMonthCalendar.get(Calendar.YEAR) + ". " +
-                (mThisMonthCalendar.get(Calendar.MONTH) + 1) + ". " +
-                mDayList.get(position).getDay();
+                month + ". " +
+                day;
+
         dbDate = mThisMonthCalendar.get(Calendar.YEAR) + "-" +
-                (mThisMonthCalendar.get(Calendar.MONTH) + 1) + "-" +
-                mDayList.get(position).getDay();
+                month + "-" +
+                day;
+
+        //일기 불러오기
+
+     /*   while (cursor.moveToNext()) {
+            if (cursor.getString(0).trim().equals(dbDate)){
+                diary.setText(cursor.getString(1));
+                diaryContainer.addView(diary);
+
+            }
+        }*/
 
         /*
             일기가 DB에 있으면 일기랑 스탬프 보여주기 !! diary.setText(~); stamp.setImage(~);
@@ -264,10 +293,8 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
     }
 
     @Override
-    public void onClick(View v)
-    {
-        switch(v.getId())
-        {
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.mleft:
                 mThisMonthCalendar = getLastMonth(mThisMonthCalendar);
                 getCalendar(mThisMonthCalendar);
@@ -279,8 +306,7 @@ public class CalenD extends AppCompatActivity implements AdapterView.OnItemClick
         }
     }
 
-    private void initCalendarAdapter()
-    {
+    private void initCalendarAdapter() {
         mCalendarAdapter = new CalendarAdapter(this, R.layout.item_view_calendar, mDayList);
         mGvCalendar.setAdapter(mCalendarAdapter);
     }
