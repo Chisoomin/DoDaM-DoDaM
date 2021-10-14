501033a1b68e3eec661a08402a282d60e7cc66c5
h2suu <h2suu@naver.com>
2021-10-14 13:19:22 +0900

    드로어, 플레이리스트 오류 수정(211014) + 약간의 수정

---
+++ app/src/main/java/com/example/dodamver3/AllReward.java
@@ -0,0 +1,14 @@
+package com.example.dodamver3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AllReward extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_all_reward );
+    }
+}
